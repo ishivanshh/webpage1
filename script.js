604e1729cf41d591e8484d53597a024c1e6b1b17
Shivansh Saxena <shivanshsaxena@Shivanshs-MacBook-Air.local>
2025-08-17 16:49:07 +0530

    changes done

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
+document.querySelectorAll("#footer .project-link").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent default link behavior
+    const repoUrl = "https://github.com/ishivanshh?tab=stars"
+    window.open(repoUrl, "_blank"); // Open in a new tab
+  });
+});
 
+// CONTACT NOW BUTTON => LINKEDIN. 
+document.querySelectorAll(".contactNow").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent default link behavior
+    const contactUrl = "https://www.linkedin.com/in/saxenashivansh/"
+    window.open(contactUrl, "_blank"); // Open in a new tab
+  });
+});
+// Open the skill card when "Skills" section is clicked
+document.querySelectorAll(".elem").forEach(elem => {
+  if (elem.innerText.toLowerCase().includes("skills")) {
+    elem.addEventListener("click", (e) => {
+      e.stopPropagation(); // stop click from bubbling up
+      document.getElementById("skillCard").classList.add("active");
+    });
+  }
+});
+
+// Close card when clicking outside it
+document.addEventListener("click", (e) => {
+  const card = document.getElementById("skillCard");
+  if (card.classList.contains("active") && !card.contains(e.target)) {
+    card.classList.remove("active");
+  }
+});
+//CONTACT LINK HANDLER
+document.querySelectorAll(".contact-link").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent default link behavior
+    const url = link.dataset.url;
+    window.open(url, "_blank"); // Open in a new tab
+  });
+});
+
+// github profile link
+document.querySelectorAll(".githubprofile").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent default link behavior
+    const githubUrl = "https://github.com/ishivanshh"
+    window.open(githubUrl, "_blank"); // Open in a new tab
+  });
+});
 
 function firstPage(){
     let t1 = gsap.timeline();
